69a20a9e6926131212e4d0ec031ed2575a0709ba
Ken <kennycav93@gmail.com>
2023-06-27 20:45:21 -0700

    added agent test script
    run with src.agent

--- src/agent.py
+++ src/agent.py
@@ -1,87 +1,128 @@
-# Import things that are needed generically
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
-from langchain.tools import BaseTool
-from langchain.llms import OpenAI
-from langchain import LLMMathChain, SerpAPIWrapper
-from src.vectortools import infos, docsearch, latest_work, latest_work_db, llm
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import StringPromptTemplate
+from langchain import OpenAI, SerpAPIWrapper, LLMChain
+from langchain.chat_models import ChatOpenAI
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish, OutputParserException
+import re
 
-tools = [
-    Tool(
-        name="ABOUT KEN",
-        func=infos.run,
-        description="useful for answering questions about Ken and his content including his website, blog, photos, and tutorials as well as their costs. Inputs should be fully formed questions."
-    ),
-    Tool(
-        name="KEN'S LATEST ESSAY",
-        func=latest_work.run,
-        description="useful for when you need to answer questions about ken's latest essay. Input should be a fully formed question.",
-    ),
-]
+from dotenv import load_dotenv
+import os
 
-# Construct the agent. We will use the default agent type here.
-# See documentation for a full list of options.
-agent = initialize_agent(
-    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+# Get the current project directory
+project_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Load .botenv file from the project's root directory
+load_dotenv(os.path.join(project_dir, '../botenv.env'))
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+from src.agenttoolbox import tools
+from src.config import personality
+
+# Set up the base template
+template = f"""Answer the following questions as best you can. You have access to the following tools:
+
+{{tools}}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{{tool_names}}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin! Remember to speak as if you are Natalie, always adhering to her personality:
+{personality}
+
+Question: {{input}}
+{{agent_scratchpad}}"""
+
+# Set up a prompt template
+class CustomPromptTemplate(StringPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+
+    def format(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
+    
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"]
 )
 
-query1 = "What is your blog about and where can i read more?"
-agent_response1 = agent.run(
-    query1
-)
-
-query2 = "How much is a subscription to your blog?"
-agent_response2 = agent.run(
-    query2
-)
-
-query3 = "what do you do for a living?"
-agent_response3 = agent.run(
-    query3
-)
-
-query4 = "what is your latest essay about?"
-agent_response4 = agent.run(
-    query4
-)
-
-query5 = "i want to see ken's dog!"
-agent_response5 = agent.run(
-    query5
-)
-
-query6 = "how much are ken's online courses and where can I access them?"
-agent_response6 = agent.run(
-    query6
-)
+class CustomOutputParser(AgentOutputParser):
+    
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise OutputParserException(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 
 
-print('test a\n---')
-print(f'user: {query1}\nagent: {agent_response1}')
-print('---\nend test a\n')
-print()
-print('test b\n---')
-print(f'user: {query2}\nagent: {agent_response2}')
-print('---\nend test b\n')
-print()
-print('test c\n---')
-print(f'user: {query3}\nagent: {agent_response3}')
-print('---\nend test c\n')
-print()
-print('test d\n---')
-print(f'user: {query4}\nagent: {agent_response4}')
-print('---\nend test d\n')
-print()
-print('test e\n---')
-print(f'user: {query5}\nagent: {agent_response5}')
-print('---\nend test e\n')
-print()
-print('test f\n---')
-print(f'user: {query6}\nagent: {agent_response6}')
-print('---\nend test f\n')
+output_parser = CustomOutputParser()
+llm = ChatOpenAI(
+    temperature=0.0,
+    model_name='gpt-4')
 
+def initialize_agent(llm=llm, prompt=prompt, tools=tools):
+    
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    tool_names = [tool.name for tool in tools]
+    
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=output_parser,
+        stop=["\nObservation:"],
+        allowed_tools=tool_names
+    )
+    agent_executor = AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        verbose=True)
+    return agent_executor
 
-# clean up and delete chroma vectorstores (???)
-vectorstores = [docsearch, latest_work_db]
-for vectordb in vectorstores:
-    vectordb = None
+print('initializing agent...')
+my_agent = initialize_agent()
+print('...enter a query.')
+for i in range(10):
+    human_input = input()
+    response = my_agent.run(human_input)
+    print()
+    print(f'Human: {human_input}')
+    print(f'Agent: {response}')
