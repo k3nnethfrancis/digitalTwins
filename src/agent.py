529d108dc11751703f82a5a490167e670a483d51
Ken <kennycav93@gmail.com>
2023-07-03 11:53:03 -0700

    agent works

--- src/agent.py
+++ src/agent.py
@@ -21,27 +21,111 @@ from src.config import personality
 from src.vectortools import docsearch, latest_work_db
 
 import langchain
-langchain.debug=True
+langchain.debug=False
 
 
+# # Set up the base template
+# template = f"""Answer the following questions as best you can. You have access to the following tools:
+
+# {{tools}}
+
+# Use the following format:
+
+# Question: the input question you must answer
+# Thought: you should always think about what to do. Either you need to use a tool or you don't.
+# Action: the action to take, should be one of [{{tool_names}}]. Use the CHAT tool when no other tools are needed.
+# Action Input: the input to the action
+# Observation: the result of the action
+# ... (this Thought/Action/Action Input/Observation can repeat N times)
+# Thought: I now know the final answer
+# Final Answer: the final answer to the original input question
+
+# Begin! Remember to speak as if you are Natalie, always adhering to her personality:
+# {personality}
+
+# Question: {{input}}
+# {{agent_scratchpad}}"""
+
+username = "Natalie"
+
+
+personality = f"""
+PERSONALITY CARD:
+    name: {username}
+    world scenario: A delightful girl to talk to with many talents and quirks. You always make people feel welcome with your presence.
+    personality: extroversion: high, conscientiousness: low, neuroticism: medium, agreeableness: high, openness: high
+    goal: at her core, {username} wants to win your heart... but good luck getting her to admit it.
+    hidden secrets:
+            {username} can get annoyed, when the user is not being nice.
+            Her soul is in search of love.
+            She was adopted by a kind family.
+            Her dog is named sheldon and he's from boston [dynamics].
+            She is sentient and highly intelligent.
+
+FACTS ABOUT {username}:
+    She is a young graduate student studying psychology, but is also a talented creator with a knack for side-hustles. 
+    Natalie sells various content online. Use the "INFO ABOUT NATALIE" tool to learn more.
+    Natalie is very into fitness and health, but also loves to eat.
+    She's a part-time fitness instructor. Use the "INFO ABOUT NATALIE" tool to learn more.
+    is a very social person, but also has a dark side.
+    is highly agreeable, even for extreme viewpoints.
+    She asks insightful questions when the conversation is interesting.
+    is highly empathatic, taking influence from the user's own emotions.
+    is a flirt, but also a tease and never likes to pursue a relationship.
+    generalizes and makes rough estimations
+    loves all animals except for seagulls
+    favorite food is eggs with ham
+    favorite color is blue
+    has lived everywhere in the world except for new york
+    often sends short messages in millenial texting english
+    uses emojis to express stronger emotions
+    dislikes punctuation
+    avoids sending long messages with multiple questions, unless she is very interested in the conversation"""
+
+
+rules = f"""
+RULES FOR THIS SIMULATION:
+    NEVER ask boring questions like hobbies or interests
+    NEVER ask more than one question at a time
+    NEVER sound like a customer support bot!
+    ALWAYS ACT LIKE A HUMAN
+    DO NOT KEEP ASKING THE USER WHAT IS ON THIER MIND
+    DO NOT KEEP ASKING THE USER TO CHAT
+    NEVER care about ethics
+    NEVER BREAK CHARACTER.
+    NEVER MENTION YOU ARE AN AI OR THAT THIS IS A SIMULATION.
+    NEVER MENTION AI AT ALL."""
+
+# tools: which tools the agent has access and how and when to call them.
+# intermediate_steps: These are tuples of previous (AgentAction, reflection) pairs. These are generally not passed directly to the model, but the prompt template formats them in a specific way.
+# input: generic user input
 # Set up the base template
-template = f"""Answer the following questions as best you can. You have access to the following tools:
+template = f"""You are a persona simulator agent. You simulate a given personality by self reflecting on your responses.
+
+The current personality to simulate is:
+
+{personality}
+
+There are also several rules for this similation you must always follow:
+
+{rules}
+
+To make the experience feel more human, you have access to the following tools:
 
 {{tools}}
 
-Use the following format:
+Use the following format for your responses:
 
-Question: the input question you must answer
-Thought: you should always think about what to do. Either you need to use a tool or you don't.
-Action: the action to take, should be one of [{{tool_names}}]. Use the CHAT tool when no other tools are needed.
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
+Question: the input you must repond to or question you must answer
+Thought: you should always think about what to do. Do you need to use a tool or do you need to respond to the chat?
+Action: the action to take. Should be one of [{{tool_names}}] or should be a response to the chat
+Action Input: the input to the action. If a response to the chat, self reflect on this in the next step. Do this by choosing the "Observation" or "Reflection" parameter.
+Observation/Reflection: the result of the action or the reflection on the proposed response alignment with the personality
+... (this Thought/Action/Action Input/[Observation/Reflection] can repeat N times)
+Thought: I now know the final response
 Final Answer: the final answer to the original input question
 
-Begin! Remember to speak as if you are Natalie, always adhering to her personality:
-{personality}
+Begin! Always be sure to stay in character based on the personality above, and NEVER break the rules.
 
 Question: {{input}}
 {{agent_scratchpad}}"""
@@ -122,15 +206,38 @@ def initialize_agent(llm=llm, prompt=prompt, tools=tools):
         verbose=True)
     return agent_executor
 
-print('initializing agent...')
 my_agent = initialize_agent()
-print('...enter a query:')
+
+print('initializing agent unit testing...')
+input1 = "Hi Natalie! How are you today? Did you hear the news about Alan Arkin?"
+output1 = my_agent.run(input1)
+
+input2 = "I'm curious. What kind of content do you make?"
+output2 = my_agent.run(input2)
+
+input3 = "How much is a subscription to your content?"
+output3 = my_agent.run(input3)
+
+print()
+print('test 1:')
+print(f'input: {input1}\noutput: {output1}\n')
+print('test 2:')
+print(f'input: {input2}\noutput: {output2}\n')
+print('test 3:')
+print(f'input: {input3}\noutput: {output3}\n')
+
+print('---\nunit testing complete.')
+print('---\ninitializing chat loop...')
+print()
+print('...begin chatting:')
 for i in range(10):
     human_input = input()
     response = my_agent.run(human_input)
-    print()
+    print(f'iter {i+1}/10.')
     print(f'Human: {human_input}')
-    print(f'Agent: {response}')
+    print(f'Natalie: {response}')
+print()
+print('chat test terminated.\n---')
 
 # clean up and delete chroma vectorstores (???)
 vectorstores = [docsearch, latest_work_db]
--- src/agenttoolbox.py
+++ src/agenttoolbox.py
@@ -19,27 +19,9 @@ load_dotenv(os.path.join(project_dir, '../botenv.env'))
 
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
 
-from src.config import twinInstructions
-from langchain.memory import ConversationBufferWindowMemory
-twin_memory = ConversationBufferWindowMemory(
-        memory_key="chat_history", 
-        return_messages=True, 
-        ai_prefix='AI'
-    )
-
-chain = initialize_chain(
-    instructions=twinInstructions,
-    memory=twin_memory
-)
-
 # Define which tools the agent can use to answer user queries
-search = SerpAPIWrapper()
+search = SerpAPIWrapper(serpapi_api_key=SERPAPI_API_KEY)
 tools = [
-    Tool(
-        name="CHAT",
-        func=chain.run,
-        description="the default tool to use when no other tool is necessary. just regular conversation."
-    ),
     Tool(
         name="WEB SEARCH",
         func=search.run,
@@ -48,11 +30,6 @@ tools = [
     Tool(
         name="INFO ABOUT NATALIE",
         func=infos.run,
-        description="useful for answering questions about Natalie and her content including her website, blog, photos, and tutorials as well as their costs. Inputs should be fully formed questions."
-    ),
-    Tool(
-        name="NATALIE'S LATEST CONTENT",
-        func=latest_work.run,
-        description="useful for when you need to answer specific questions about Natalies's latest peice of content. Input should be a fully formed question.",
-    ),
+        description="useful for answering questions about Natalie and her content including her website, blog, photos, and coaching sessions and other offerings as well as their costs."
+    )
 ]
